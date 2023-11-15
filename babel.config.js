ff28907570039ae4199b7b4be910e07e1b37911f
codeauthor <codeauthor2000@gmail.com>
2023-11-15 02:24:00 +0100

    chore: config project

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,37 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
     plugins: [
       // Required for expo-router
-      'expo-router/babel',
+      "expo-router/babel",
     ],
+    env: {
+      production: {
+        plugins: [
+          "react-native-paper/babel",
+          [
+            "module:react-native-dotenv",
+            {
+              envName: "APP_ENV",
+              moduleName: "@env",
+              path: ".env",
+            },
+          ],
+          [
+            "module-resolver",
+            {
+              root: ["./src"],
+              alias: {
+                "@widgets": "./src/widgets/",
+                "@hooks": "./hooks/",
+                "@apis": "./src/apis/",
+                types: "./src/types/",
+              },
+            },
+          ],
+        ],
+      },
+    },
   };
 };
